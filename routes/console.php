50605288a97a61983baf76340a5282a16642581f
Nikolay I. Dolzhnikov <n.dolzhnikov@gmail.com>
2025-11-16 16:41:58 +0300

    Доработки

--- routes/console.php
+++ routes/console.php
@@ -9,4 +9,13 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
+Artisan::command('tenders:generate-auto {count=1}', function (int $count) {
+    for ($i = 0; $i < $count; $i++) {
+        $this->info('Запуск генерации тендера через DeepSeek (попытка ' . ($i + 1) . ')');
+        GenerateAutoTenderJob::dispatch();
+    }
+
+    $this->info('Команда генерации тендеров отправлена в очередь.');
+})->purpose('Ручной запуск генерации тендеров через DeepSeek');
+
 Schedule::job(GenerateAutoTenderJob::class)->hourly();
